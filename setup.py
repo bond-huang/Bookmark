684e11fe467a53504748e2b86d6d15eaeb84a5af
bond-huang <58139438+bond-huang@users.noreply.github.com>
2021-03-14 16:19:04 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,41 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages, setup, Command
+
+import subprocess
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        errno = subprocess.call(['py.test'])
+        raise SystemExit(errno)
+
 
 setup(
     name='nav',
     version='1.0.0',
+    url='http://github.com/bond-huang/navigator',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         'flask',
     ],
+    cmdclass={'test': PyTest},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 )
